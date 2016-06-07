69a54f3ab3ed6bfef21c33ff58739fce13b0a262
Mark Solters <msolters@gmail.com>
2016-06-06 21:42:00 -0400

    Created branch containing changes required to use srf06 emk board

--- examples/cc26xx-ota-bootloader/ota-image-example/project-conf.h
+++ examples/cc26xx-ota-bootloader/ota-image-example/project-conf.h
@@ -3,3 +3,10 @@
 
 #undef REST_MAX_CHUNK_SIZE
 #define REST_MAX_CHUNK_SIZE            256
+
+//  srf06 spi pins
+#define BOARD_IOID_FLASH_CS       IOID_14
+#define BOARD_FLASH_CS            (1 << BOARD_IOID_FLASH_CS)
+#define BOARD_IOID_SPI_CLK_FLASH  IOID_10
+#define BOARD_IOID_SPI_MOSI       IOID_9
+#define BOARD_IOID_SPI_MISO       IOID_8
